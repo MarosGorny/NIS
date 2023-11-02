f8f0d790afa44fc9adbfaa4add1e3142ad127ca3
DjSeanza <ghostikk2@gmail.com>
2023-11-02 19:32:43 +0100

    login updated for new database, logout added

--- nis-client/src/App.jsx
+++ nis-client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import './App.scss';
 import { Login } from './auth/login/Login';
+import Logout from './auth/logout/Logout';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/logout" element={<Logout></Logout>}></Route>
       </Routes>
     </div>
   );
