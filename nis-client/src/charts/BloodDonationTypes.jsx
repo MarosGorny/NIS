9fb248c9aabcc2cbc2af272924d4b9190844620b
Maros Gorny <maros.gorny@gmail.com>
2023-11-29 14:15:38 +0100

    Refactor BloodDonationTypes component to card

--- nis-client/src/charts/BloodDonationTypes.jsx
+++ nis-client/src/charts/BloodDonationTypes.jsx
@@ -2,10 +2,10 @@
 import GetUserData from 'auth/get_user_data';
 import { Chart } from 'primereact/chart';
 import React, { useEffect, useState } from 'react';
+import { Card } from 'primereact/card';
 
 export default function BloodDonationTypes(props) {
   const [chartData, setChartData] = useState({});
-  const [chartOptions, setChartOptions] = useState({});
   const [bloodTypes, setBloodTypes] = useState({});
 
   useEffect(() => {
@@ -26,46 +26,58 @@ export default function BloodDonationTypes(props) {
       datasets: [
         {
           data: bloodData,
+          backgroundColor: [
+            "#42A5F5",
+            "#66BB6A",
+            "#FFA726",
+            "#26C6DA",
+            "#7E57C2",
+            "#FF7043",
+            "#EC407A",
+            "#8D6E63"
+          ],
         },
       ],
     };
-    const options = {
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
-          },
-        },
-      },
-    };
 
     setChartData(data);
-    setChartOptions(options);
   }, [bloodTypes]);
 
   const fetchBloodTypeData = () => {
-    const token = localStorage.getItem('logged-user');
-    const headers = { authorization: 'Bearer ' + token };
-    const userData = GetUserData(token);
-    const hospitalId = props?.hospitalId || userData.UserInfo.hospital;
-    const date = props?.date || new Date();
-    fetch(`/dashboard/bloodTypesInDonations/${hospitalId}/date/${date}`, {
-      headers,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setBloodTypes(data);
-      });
+      const token = localStorage.getItem('logged-user');
+      const headers = { authorization: 'Bearer ' + token };
+      const userData = GetUserData(token);
+      const hospitalId = props?.hospitalId || userData.UserInfo.hospital;
+      const date = props?.date || new Date();
+      fetch(`/dashboard/bloodTypesInDonations/${hospitalId}/date/${date}`, {
+        headers,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setBloodTypes(data);
+        });
+  };
+
+  const chartOptions = {
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+        },
+      },
+    },
+    maintainAspectRatio: false,
   };
 
   return (
-    <div className="card flex justify-content-center">
-      <Chart
-        type="pie"
-        data={chartData}
-        options={chartOptions}
-        className="w-full md:w-30rem"
-      />
-    </div>
+    <Card title="Typy darovanej krvi" className="flex justify-content-center">
+      <div style={{ width: '100%', maxWidth: '500px', minHeight: '300px' }}>
+        <Chart
+          type="pie"
+          data={chartData}
+          options={chartOptions}
+        />
+      </div>
+    </Card>
   );
 }
